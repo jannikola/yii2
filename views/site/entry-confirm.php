0b5778bae16de77482736f91960961b71dea47ed
jannikola <jannikola@gmail.com>
2018-10-11 23:19:53 +0200

    uploading files

---
+++ views/site/entry-confirm.php
@@ -0,0 +1,8 @@
+<?php
+use yii\helpers\Html;
+?>
+<p>You have entered the following information:</p>
+<ul>
+    <li><label>Name: </label><?= Html::encode($model->name); ?></li>
+    <li><label>email: </label><?= Html::encode($model->email) ?></li>
+</ul>
